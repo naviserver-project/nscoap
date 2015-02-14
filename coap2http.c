7198c3e6896a6ca41a466f707bedd195a8f43a77
david@david-t440s.david.sites.hopfmueller.at <david@david-t440s.david.sites.hopfmueller.at>
2015-02-14 15:22:15 +0100

    Cleanup code, complete option parsing

--- coap2http.c
+++ coap2http.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <tkDecls.h>
 
 // Maximum size (bytes) of a CoAP message as suggested by RFC 7252
 #define MAX_SIZE 1152
@@ -9,20 +11,40 @@
 // Beginning of declarations (will be moved to .h later)
 
 typedef unsigned char byte;
+typedef unsigned char bool;
 /*
-* Holds the request and its parameters
-*/
+ * Message code details
+ */
+typedef struct Code_s
+{
+    int     class;
+    int     detail;
+} Code_t;
+typedef struct Option_s
+{
+    int     number;
+    int     length;
+    byte    *value;
+} Option_t;
+/*
+ * Holds the request and its parameters
+ */
 typedef struct Request_s
 {
-    byte    raw[MAX_SIZE];  // holds the raw packet
-    int     size;           // size in bytes
-    int     valid;          // validity of the request
-    int     version;        // CoAP version
-    int     type;           // message type
-    int     tkl;            // token length
-    char    *code;          // message code
-    char    *token;         // token
-    byte    *payload;       // payload
+    byte        raw[MAX_SIZE];  // holds the raw packet
+    int         size;           // size in bytes
+    int         valid;          // validity of the request
+    int         position;       // current parser position
+    int         version;        // CoAP version
+    int         type;           // message type
+    int         tkl;            // token length
+    Code_t      *code;          // message code
+    byte        *mid;           // message id
+    byte        *token;         // token
+    byte        *payload;       // payload
+    int         payloadLength;  // length of payload
+    int         optionCount;    // number of valid options
+    Option_t    *options[MAX_SIZE - 4];
 } Request_t;
 
 static int loadMessage(char *file, Request_t *request);
@@ -32,14 +54,9 @@ static int parseRequest(Request_t *request);
 
 int main(int argc, char *argv[])
 {
-    Request_t request = { 0, 0, 0, 0, 0, 0, 0 }, rPtr = &request; // hm…
-    request.valid = 1;
-
+    Request_t request = NULL;
     bzero(&request, sizeof(Request_t));
-    request.payload = NULL;
-    rPtr-.payload = "lfhl";
-
-
+    request.valid = 1;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: coap2http <captured.file>\n");
@@ -60,7 +77,7 @@ static int loadMessage(char *file, Request_t *request)
     FILE *fd;
 
     if ((fd = fopen(file, "r"))) {
-        request->size = fread(request->raw, 1, MAX_SIZE, fd);
+        request->size = (int) fread(request->raw, 1, MAX_SIZE, fd);
         fclose(fd);
         return 1;
     } else
@@ -71,12 +88,9 @@ static int loadMessage(char *file, Request_t *request)
 /*
  * Check size of the CoAP message
  */
-static int checkMessageSize(Request_t *request, int increment)
+static bool checkRemainingSize(Request_t *request, int increment)
 {
-    // Set initial value, remember accross intantiations
-    static int currentSize;
-    currentSize += increment;
-    if (request->size >= currentSize) {
+    if ((request->position + increment) > request->size) {
         request->valid = 0;
         return 0;
     }
@@ -86,142 +100,145 @@ static int checkMessageSize(Request_t *request, int increment)
 /*
  * Parse the content of a CoAP request
  */
-static int parseRequest(Request_t *request)
-{
+static int parseRequest(Request_t *request) {
     // Loop variable
     int i;
     // Index of next byte to parse
-    int field_idx = 0;
-    // Current option
-    int option = 0;
+    request->position = 0;
+    // Option struct
+    Option_t option;
+    bzero(&option, sizeof(Option_t));
     // Last option delta
-    int last_option = 0;
-    // Length
-    int length = 0;
+    int lastOptionNumber = 0;
     // Registry of valid CoAP message codes
-    typedef struct Code {
-        int class;
-        int detail;
-    }
-    int codes[] = {
-            000,
-            001,
-            002,
-            ...
-            205,
-            400,
-
-    }
-    char *codes[] =
-            {
-                    "0.00",
-                    "0.01",
-                    "0.02",
-                    "0.03",
-                    "0.04",
-                    "2.01",
-                    "2.02",
-                    "2.03",
-                    "2.04",
-                    "2.05",
-                    "4.00",
-                    "4.01",
-                    "4.02",
-                    "4.03",
-                    "4.04",
-                    "4.05",
-                    "4.06",
-                    "4.12",
-                    "4.13",
-                    "4.15",
-                    "5.00",
-                    "5.01",
-                    "5.02",
-                    "5.03",
-                    "5.04",
-                    "5.05"
-            };
+    int code_registry[] = {
+            000, 001, 002, 003, 004,
+            201, 202, 203, 204, 205,
+            400, 401, 402, 403, 404, 405, 406, 412, 413, 415,
+            500, 501, 502, 503, 504, 505
+    };
     // CoAP messages can't be shorter than 5 bytes
-    if (!checkMessageSize(request, 4))
+    if (!checkRemainingSize(request, 4))
         return 0;
     // CoAP version: must be 0b01
     // MSB must be 0 anyways, so >> should be safe
-    request->version = ((request->raw[0] >> 6) == 0x1);
+    request->version = ((request->raw[0] >> 6) == 0x1u);
     if (!request->version) {
         request->valid = 0;
         return 0;
     }
     // message type: 0..3
-    request->type = ((request->raw[0] & 0x30) >> 4);
+    request->type = ((request->raw[0] & 0x30u) >> 4);
     // token length: 0..8
-    request->tkl = (request->raw[0] & 0xf);
+    request->tkl = (request->raw[0] & 0xfu);
     if (request->tkl > 8) {
         request->valid = 0;
         return 0;
     }
     // CoAP code
-    char code_raw = request->raw[1];
-    char code[4];
-    char class = (((code_raw & 0xe0u) >> 5) & (~0x7));
-    char detail = ((code_raw & 0x1fu) | 0x00);
-    request->statusCode = class * 100 + detail;
-    snprintf(code, sizeof code, "%d.%02d", class, detail);
-    for (i = 0; i < (sizeof codes / sizeof(char *)); i++) {
-        if (codes[i] == code) {
-            request->code = &code[0];
+    byte code_raw = request->raw[1];
+    Code_t code;
+    code.class = (((code_raw & 0xe0u) >> 5) & (~0x7u));
+    code.detail = ((code_raw & 0x1fu) | 0x00u);
+    for (i = 0; i < (sizeof code_registry / sizeof(int)); i++) {
+        if (code_registry[i] == (code.class * 100 + code.detail)) {
+            request->code = &code;
             break;
         }
     }
-    // Did we find a valid code found?
-    if (*(request->code) == 0)
+    // Did we find a valid code?
+    if (request->code == 0)
         return 0;
-    return 1;
     // Message ID
-    strncopy(request->mid, request->raw[2], 2);
+    request->mid = &(request->raw[2]);
     // Token
-    field_idx = 4;
+    request->position = 4;
     if (request->tkl > 0) {
-        if (checkMessageSize(request, request->tkl)) {
-            strncpy(request->token, request->raw[4], request->tkl);
-            field_idx += request->tkl;
+        if (checkRemainingSize(request, request->tkl)) {
+            request->token = &(request->raw[4]);
+            request->position += request->tkl;
         } else {
             return 0;
         }
     }
     // Options
-    processOptions = (request->size > field_idx);
+    bool processOptions = checkRemainingSize(request, 1);
     while (processOptions) {
-        option = (int)(((request->raw[field_idx] & 0xf0) >> 4) & 0x0f);
-        length = (int)(request->raw[field_idx] & 0x0f);
+        request->position += 1;
+        option.number = (((request->raw[request->position] & 0xf0u) >> 4) & 0x0fu);
+        option.length = (request->raw[request->position] & 0x0fu);
         // Parse option delta
-        switch (option) {
+        switch (option.number) {
             case 0x0fu:
-                // Handle payload
-                if (length = 0x0fu) {
-                    request->payloadLength = ...;
-                    request->payload = request->raw[field_idx+1]
-                    processOptions = 0;
-                    break;
-                    case 14:
-                        option
-                        break;
-                    case 13:
+                // Payload marker or invalid
+                switch (option.length) {
+                    case 0x0fu:
+                        request->payload = &(request->raw[request->position]);
+                        request->payloadLength = request->size - request->position;
                         break;
                     default:
-                        option += last_option;
-                    break;
+                        request->valid = 0;
+                        break;
                 }
-                //finshed = request->size > field_idx;
+                processOptions = 0;
+            case 0x0eu:
+                if (checkRemainingSize(request, 2)) {
+                    option.number = (int) request->raw[request->position] * 256 + (int) request->raw[request->position + 1] - 269;
+                    request->position += 2;
+                }
+                break;
+            case 0x0du:
+                if (checkRemainingSize(request, 1)) {
+                    option.number = (int) request->raw[request->position ] - 13;
+                    request->position += 1;
+                }
+                break;
+            default:
+                break;
         }
-        finished:
-
+        // No payload, process length
+        if (processOptions) {
+            switch (option.length) {
+                case 0x0fu:
+                    // Invalid
+                    request->valid = 0;
+                    processOptions = 0;
+                case 0x0eu:
+                    if (checkRemainingSize(request, 2)) {
+                        option.length = (int) request->raw[request->position] * 256 + (int) request->raw[request->position + 1] - 269;
+                        request->position += 2;
+                    }
+                case 0x0du:
+                    if (checkRemainingSize(request, 1)) {
+                        option.length = (int) request->raw[request->position ] - 13;
+                        request->position += 1;
+                    }
+                default:
+                    break;
+            }
+        }
+        option.number += lastOptionNumber;
+        lastOptionNumber = option.number;
+        if (processOptions) {
+            if (option.length > 0) {
+                if (checkRemainingSize(request, option.length)) {
+                    option.value = &(request->raw[request->position]);
+                } else {
+                    // Invalid
+                    request->valid = 0;
+                    processOptions = 0;
+                }
+            }
+        }
+        if (processOptions) {
+            // Append option to collection
+            request->options[request->optionCount] = &option;
+            request->optionCount++;
+            if (!checkRemainingSize(request, 1)) processOptions = 0;
+        }
     }
-
+    return request->valid;
+}
 /*
- * Fragen:
- * - Warum können den struct-ints Werte ohne cast zugewiesen werden?
- * - "At top level"?
- * - *request->code?
- * - Substitution * []
- * - \0 notwendig?
+ *
  */
