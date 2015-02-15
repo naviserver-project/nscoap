4a7bb607a4c843893308f9314f406aa6aabf7cd5
Gustaf Neumann <neumann@wu-wien.ac.at>
2015-02-15 13:27:56 +0100

    - follow naviserver conventions - some cleanup

--- coap2http.c
+++ coap2http.c
@@ -1,19 +1,25 @@
-// CoAP to HTTP converter, standalone version
+/*
+ * CoAP to HTTP converter, standalone version
+ */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <ns.h>
 
-// Control debug output
+/*
+ * Control debug output
+ */
 #define DEBUG 1
 
-// Maximum size (bytes) of a CoAP message as suggested by RFC 7252
+/*
+ * Maximum size (bytes) of a CoAP message as suggested by RFC 7252
+ */
 #define MAX_SIZE 1152
 
-// Beginning of declarations (will be moved to .h later)
+/*
+ * Beginning of declarations (will be moved to .h later)
+ */
 
 typedef unsigned char byte;
-typedef unsigned char bool;
+
 /*
  * Message code details
  */
@@ -22,277 +28,349 @@ typedef struct Code_s
     int     class;
     int     detail;
 } Code_t;
+
 typedef struct Option_s
 {
-    int     number;
-    int     length;
-    byte    *value;
+    unsigned int delta;
+    unsigned int length;
+    byte        *value;
 } Option_t;
+
 /*
  * Holds the request and its parameters
  */
 typedef struct Request_s
 {
-    byte        raw[MAX_SIZE];  // holds the raw packet
-    int         size;           // size in bytes
-    int         valid;          // validity of the request
-    int         position;       // current parser position
-    int         version;        // CoAP version
-    int         type;           // message type
-    int         tkl;            // token length
-    Code_t      *code;          // message code
-    int         mid;            // message id
-    byte        *token;         // token
-    byte        *payload;       // payload
-    int         payloadLength;  // length of payload
-    int         optionCount;    // number of valid options
-    Option_t    *options[MAX_SIZE - 4];
+    byte        raw[MAX_SIZE];  /* holds the raw packet */
+    int         size;           /* size in bytes */
+    bool        valid;          /* validity of the request */
+    int         position;       /* current parser position */
+    int         version;        /* CoAP version */
+    int         type;           /* message type */
+    int         tokenLength;    /* token length */
+    int         codeValue;      /* message code */
+    int         messageID;      /* message id */
+    byte        *token;         /* token */
+    byte        *payload;       /* payload */
+    int         payloadLength;  /* length of payload */
+    int         optionCount;    /* number of valid options */
+    Option_t    options[MAX_SIZE - 4];
 } Request_t;
 
-static int loadMessage(char *file, Request_t *request);
-static int parseRequest(Request_t *request);
+static bool LoadMessage(char *file, Request_t *request);
+static bool ParseRequest(Request_t *request);
 
-// End of declarations
+/*
+ * End of declarations
+ */
 
 int main(int argc, char *argv[])
 {
     Request_t request;
+
     bzero(&request, sizeof(Request_t));
-    request.valid = 1;
+    request.valid = NS_TRUE;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: coap2http <captured.file>\n");
         exit(-1);
     }
-    if (loadMessage(argv[1], &request)) {
-        printf("Result: %d\n", parseRequest(&request));
+    if (LoadMessage(argv[1], &request) == NS_TRUE) {
+        printf("Result: %d\n", ParseRequest(&request));
     }
-    return EXIT_SUCCESS;
+
+    return 0;
 }
 
 /*
  * Load message from file and save it to buffer together with its size
  */
-static int loadMessage(char *file, Request_t *request)
+static bool LoadMessage(char *file, Request_t *request)
 {
     FILE *fd;
+    bool success = NS_TRUE;
 
     if ((fd = fopen(file, "r"))) {
         request->size = (int) fread(request->raw, 1, MAX_SIZE, fd);
         fclose(fd);
-        return 1;
-    } else
+    } else {
         fprintf(stderr, "File <%s> could not be opened.\n", file);
-    return 0;
+        success = NS_FALSE;
+    }
+
+    return success;
 }
 
 /*
  * Check size of the CoAP message
  */
-static bool checkRemainingSize(Request_t *request, int increment)
+static bool CheckRemainingSize(Request_t *request, int increment)
 {
+    bool success = NS_TRUE;
+
     if ((request->position + increment) > request->size) {
-        request->valid = 0;
-        return 0;
+        request->valid = NS_FALSE;
+        success = NS_FALSE;
     }
-    return 1;
+
+    return success;
 }
 
 /*
  * Parse the content of a CoAP request
  */
-static int parseRequest(Request_t *request) {
-    // Loop variable
-    int i;
-    // Index of next byte to parse
-    request->position = 0;
-    // Option struct
+static bool ParseRequest(Request_t *request) {
     Option_t option;
-    bzero(&option, sizeof(Option_t));
-    request->valid = 1;
-    // Last option delta
-    int lastOptionNumber = 0;
-    // Registry of valid CoAP message codes
-    int code_registry[] = {
-            000, 001, 002, 003, 004,
-            201, 202, 203, 204, 205,
-            400, 401, 402, 403, 404, 405, 406, 412, 413, 415,
-            500, 501, 502, 503, 504, 505
+    int      i, codeValue, lastOptionNumber = 0;
+    bool     processOptions;
+    Code_t   code;
+    /*
+     * Registry of valid CoAP message codes
+     */
+    static const int code_registry[] = {
+        000, 001, 002, 003, 004,
+        201, 202, 203, 204, 205,
+        400, 401, 402, 403, 404, 405, 406, 412, 413, 415,
+        500, 501, 502, 503, 504, 505
     };
+
+    request->position = 0;
+    bzero(&option, sizeof(Option_t));
+    request->valid = NS_TRUE;
+
 #ifdef DEBUG
     printf("parseRequest started.\n");
 #endif
-    // CoAP messages can't be shorter than 4 bytes
-    if (!checkRemainingSize(request, 4)) {
+    /*
+     * CoAP messages can't be shorter than 4 bytes
+     */
+    if (CheckRemainingSize(request, 4) == NS_FALSE) {
 #ifdef DEBUG
         printf("Message shorter than 4 bytes.\n");
 #endif
-        return 0;
+        return NS_FALSE;
     }
-    // CoAP version: must be 0b01
-    // MSB must be 0 anyways, so >> should be safe
+
+    /*
+     * Bit 0-1: CoAP version: Must be 0b01
+     */
     request->version = ((request->raw[0] >> 6) == 0x1u);
 #ifdef DEBUG
-    printf("CoAP version: %d\n", request->version);
+    fprintf(stderr, "CoAP version: %d\n", request->version);
+#endif
+    if (request->version == 0) {
+        request->valid = NS_FALSE;
+        return NS_FALSE;
+    }
+
+    /*
+     * Bit 2-3: Type of CoAP message
+     */
+    request->type = ((request->raw[0] >> 4) & 0x30u);
+#ifdef DEBUG
+    fprintf(stderr, "Message type: %d\n", request->type);
 #endif
 
-    if (!request->version) {
-        request->valid = 0;
-        return 0;
-    }
-    // message type: 0..3
-    request->type = ((request->raw[0] & 0x30u) >> 4);
+    /*
+     * Bit 4-7: Token Length
+     */
+    request->tokenLength = (request->raw[0] & 0x0fu);
 #ifdef DEBUG
-    printf("Message type: %d\n", request->type);
-#endif
-    // token length: 0..8
-    request->tkl = (request->raw[0] & 0xfu);
-#ifdef DEBUG
-    printf("Token length: %d\n", request->tkl);
+    fprintf(stderr, "Token length: %d\n", request->tokenLength);
 #endif
 
-    if (request->tkl > 8) {
-        request->valid = 0;
-        return 0;
+    if (request->tokenLength > 8) {
+        request->valid = NS_FALSE;
+        return NS_FALSE;
     }
-    // CoAP code
-    Code_t code;
-    int code_combined = 0;
-    byte code_raw = request->raw[1];
-    code.class = ((code_raw >> 5) & 0x7u);
-    code.detail = ((code_raw & 0x1fu) | 0x00u);
-    code_combined = (code.class * 100 + code.detail);
+
+    /*
+     * Bit 8-15: Token Length
+     */
+    code.class  = ((request->raw[1] >> 5) & 0x7u);
+    code.detail = (request->raw[1] & 0x1fu);
+    codeValue   = code.class * 100 + code.detail;
 #ifdef DEBUG
-    printf("Message code: %d\n", code_combined);
+    printf("Message code: %d\n", codeValue);
 #endif
-    for (i = 0; i < (sizeof code_registry / sizeof(int)); i++) {
-        if (code_registry[i] == code_combined) {
-            request->code = &code;
+
+    /*
+     * Check, if the code belongs to the codes defined by the CoAP RFC.  If
+     * not, we report the request being not valid (which might be a little
+     * harsh for some applications).
+     */
+    for (i = 0; i < (sizeof(code_registry) / sizeof(int)); i++) {
+        if (code_registry[i] == codeValue) {
+            request->codeValue = codeValue;
             break;
         }
     }
-    // Did we find a valid code?
-    if (request->code == 0)
-        return 0;
-    // Message ID
-    request->mid = ((int)request->raw[2]) * 256 + ((int)request->raw[3]);
+    if (request->codeValue == 0) {
+        return NS_FALSE;
+    }
+
+    /*
+     * Bit 16-31: Message ID
+     */
+    request->messageID = ((unsigned int)request->raw[2] << 8) + (unsigned int)request->raw[3];
 #ifdef DEBUG
-    printf("MID: %d\n", request->mid);
+    fprintf(stderr, "messageID: %d\n", request->messageID);
 #endif
-    // Token
+
+    /*
+     * Bit 32ff: Token
+     */
     request->position = 4;
-    if (request->tkl > 0) {
-        if (checkRemainingSize(request, request->tkl)) {
+    if (request->tokenLength > 0) {
+        if (CheckRemainingSize(request, request->tokenLength) == NS_TRUE) {
             request->token = &(request->raw[4]);
-            request->position += request->tkl;
+            request->position += request->tokenLength;
 #ifdef DEBUG
-            printf("Valid token found.\n");
+            fprintf(stderr, "Valid token found.\n");
 #endif
         } else {
-            return 0;
+            return NS_FALSE;
         }
     }
-    // Options
-    bool processOptions = checkRemainingSize(request, 1);
+
+    /*
+     * Options. Option Numbers are maintained in the "CoAP Option Numbers"
+     * registry.
+     */
+    processOptions = CheckRemainingSize(request, 1);
+
     while (processOptions) {
-        option.number = (((request->raw[request->position] & 0xf0u) >> 4) & 0x0fu);
+        /*
+         * Option Bits:
+         *    0-3: Option Delta
+         *    4-7: Option Length
+         *    8-15: Option Delta extended (8 bit,  when delta is 0x0d)
+         *    8-23: Option Delta extended (16 bit, when delta is 0x0e)
+         *    Option Length extended (8 bit, when length is 0x0d; 16 bit, when length is 0x0e)
+         *    Option Value
+         */
+        option.delta = ((request->raw[request->position] >> 4) & 0x0fu);
         option.length = (request->raw[request->position] & 0x0fu);
         request->position++;
 #ifdef DEBUG
-        printf("Processing option: number delta = %d, length = %d.\n", option.number, option.length);
+        printf("Processing option: number delta = %u, length = %u.\n",
+               option.delta, option.length);
 #endif
-        // Parse option delta
-        switch (option.number) {
+        /*
+         * Parse option delta
+         */
+        switch (option.delta) {
+            
+        case 0x0fu:
+            /*
+             * Payload marker or invalid
+             */
+            switch (option.length) {
             case 0x0fu:
-                // Payload marker or invalid
-                switch (option.length) {
-                    case 0x0fu:
-                        request->payload = &(request->raw[request->position]);
-                        request->payloadLength = request->size - request->position;
+                request->payload = &(request->raw[request->position]);
+                request->payloadLength = request->size - request->position;
 #ifdef DEBUG
-                        printf("Payload marker detected. Payload length = %d.\n", request->payloadLength);
+                fprintf(stderr, "Payload marker detected. Payload length = %d.\n",
+                        request->payloadLength);
 #endif
-                        break;
-                    default:
-                        request->valid = 0;
-                        break;
-                }
+                break;
+            default:
+                request->valid = NS_FALSE;
+                break;
+            }
+            processOptions = 0;
+            break;
+
+        case 0x0eu:
+            if (CheckRemainingSize(request, 2) == NS_TRUE) {
+                option.delta =
+                    ((unsigned int)request->raw[request->position] >> 8) +
+                    ((unsigned int)request->raw[request->position + 1] - 269);
+                request->position += 2;
+            }
+            break;
+            
+        case 0x0du:
+            if (CheckRemainingSize(request, 1) == NS_TRUE) {
+                option.delta = (int) request->raw[request->position] - 13;
+                request->position += 1;
+            }
+            break;
+            
+        default:
+            break;
+        }
+
+        /*
+         * No payload, process length
+         */
+        if (processOptions) {
+            switch (option.length) {
+                
+            case 0x0fu:
+                request->valid = NS_FALSE;
                 processOptions = 0;
                 break;
+                
             case 0x0eu:
-                if (checkRemainingSize(request, 2)) {
-                    option.number = (int) request->raw[request->position] * 256 + (int) request->raw[request->position + 1] - 269;
+                if (CheckRemainingSize(request, 2) == NS_TRUE) {
+                    option.length =
+                        ((unsigned int) request->raw[request->position] >> 8) +
+                        ((unsigned int) request->raw[request->position + 1] - 269);
                     request->position += 2;
                 }
                 break;
+                
             case 0x0du:
-                if (checkRemainingSize(request, 1)) {
-                    option.number = (int) request->raw[request->position ] - 13;
+                if (CheckRemainingSize(request, 1) == NS_TRUE) {
+                    option.length = (int) request->raw[request->position] - 13;
                     request->position += 1;
                 }
                 break;
+                
             default:
                 break;
-        }
-        // No payload, process length
-        if (processOptions) {
-            switch (option.length) {
-                case 0x0fu:
-                    // Invalid
-                    request->valid = 0;
-                    processOptions = 0;
-                    break;
-                case 0x0eu:
-                    if (checkRemainingSize(request, 2)) {
-                        option.length = (int) request->raw[request->position] * 256 + (int) request->raw[request->position + 1] - 269;
-                        request->position += 2;
-                    }
-                    break;
-                case 0x0du:
-                    if (checkRemainingSize(request, 1)) {
-                        option.length = (int) request->raw[request->position ] - 13;
-                        request->position += 1;
-                    }
-                    break;
-                default:
-                    break;
             }
         }
-        option.number += lastOptionNumber;
-        lastOptionNumber = option.number;
+        
+        option.delta += lastOptionNumber;
+        lastOptionNumber = option.delta;
 #ifdef DEBUG
-        printf("Final option number = %d, length = %d.\n", option.number, option.length);
+        fprintf(stderr, "Final option number = %u, length = %u.\n",
+                option.delta, option.length);
 #endif
+
         if (processOptions) {
             if (option.length > 0) {
 #ifdef DEBUG
-                printf("Option value expected … ");
+                fprintf(stderr, "Option value expected … ");
 #endif
-                if (checkRemainingSize(request, option.length)) {
+                if (CheckRemainingSize(request, option.length) == NS_TRUE) {
 #ifdef DEBUG
-                    printf("found.\n");
+                    fprintf(stderr, "found.\n");
 #endif
                     option.value = &(request->raw[request->position]);
                     request->position += option.length;
                 } else {
 #ifdef DEBUG
-                    printf("NOT found.\n");
+                    fprintf(stderr, "NOT found.\n");
 #endif
-                    // Invalid
-                    request->valid = 0;
+                    request->valid = NS_FALSE;
                     processOptions = 0;
                 }
             }
         }
         if (processOptions) {
-            // Append option to collection
-            request->options[request->optionCount] = &option;
+            /*
+             * Append option to collection
+             */
+            request->options[request->optionCount] = option;
             request->optionCount++;
 #ifdef DEBUG
             printf("Added option to collection.\n");
 #endif
 
-            if (!checkRemainingSize(request, 1)) {
+            if (CheckRemainingSize(request, 1) == NS_FALSE) {
 #ifdef DEBUG
                 printf("No further options/payload.\n");
 #endif
@@ -300,15 +378,21 @@ static int parseRequest(Request_t *request) {
             }
         }
 #ifdef DEBUG
-    printf("Finished parsing option/payload.\n");
+        fprintf(stderr, "Finished parsing option/payload.\n");
 #endif
 
     }
 #ifdef DEBUG
-    printf("parseRequest finished.\n");
+    fprintf(stderr, "parseRequest finished.\n");
 #endif
     return request->valid;
 }
+
 /*
- *
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * fill-column: 78
+ * indent-tabs-mode: nil
+ * End:
  */
